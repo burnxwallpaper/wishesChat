9f2ac91a3f8634e4425bd4649058779282e5ea95
leosiu wish <55885280+burnxwallpaper@users.noreply.github.com>
2020-04-01 15:11:02 +0800

    input required

--- src/Common/InputBox.js
+++ src/Common/InputBox.js
@@ -4,7 +4,7 @@ function InputBox({ handleSubmit }) {
 
     return (
         <form className="InputBox" onSubmit={handleSubmit} autoComplete="off">
-            <input id="inputMessage" ></input>
+            <input id="inputMessage" required></input>
             <label htmlFor="InputBoxSubmit" className="InputBoxSubmitLabel" ><i className="fas fa-arrow-alt-circle-right"></i></label>
             <input id="InputBoxSubmit" className="InputBoxSubmit" type="submit" value="Submit"></input>
         </form>
